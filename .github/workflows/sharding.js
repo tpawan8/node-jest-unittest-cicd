5655d53182358eec8b4b392a94aee66b80b608e3
tpawan8 <63823624+tpawan8@users.noreply.github.com>
2024-05-20 00:27:29 -0700

    test

--- .github/workflows/sharding.js
+++ .github/workflows/sharding.js
@@ -1,38 +1,39 @@
 function shardTests(tests, maxShardDuration) {
-    //console.log("tests: ");
-    logObj(tests);
-    tests = JSON.parse(tests);
-    //console.log("maxShardDuration: "+ maxShardDuration);
-    const shards = [];
-    let currentShard = { tests: [], duration: 0 };
-  
-    for (const test of tests) {
-        logObj(test);
-        const newDuration = currentShard.duration + test.duration;
-      if (newDuration > maxShardDuration) {
-        shards.push(currentShard);
-        currentShard = { tests: [], duration: 0 };
-      }
-      currentShard.tests.push(test.test); // Store only the test name
-      currentShard.duration += test.duration;
-      //console.log("currentShard:");
-      logObj(currentShard);
-    }
-  
-    // Add the last shard if it has tests
-    if (currentShard.tests.length > 0) {
+  //console.log("tests: ");
+  //logObj(tests);
+  tests = JSON.parse(tests);
+  //console.log("maxShardDuration: "+ maxShardDuration);
+  const shards = [];
+  var index = 1;
+  let currentShard = { tests: [], duration: 0, index: index };
+
+  for (const test of tests) {
+    //logObj(test);
+    const newDuration = currentShard.duration + test.duration;
+    if (newDuration > maxShardDuration) {
       shards.push(currentShard);
+      index++;
+      currentShard = { tests: [], duration: 0, index: index };
     }
-    
-    //console.log("shards");
-    logObj(shards);
-    return shards;
+    currentShard.tests.push(test.test); // Store only the test name
+    currentShard.duration += test.duration;
+    //console.log("currentShard:");
+    //logObj(currentShard);
   }
 
-  function logObj(obj)
-  {
-    //console.log(JSON.stringify(obj));
-  } 
+  // Add the last shard if it has tests
+  if (currentShard.tests.length > 0) {
+    shards.push(currentShard);
+    index++;
+  }
+
+  logObj(shards);
+  return shards;
+}
+
+function logObj(obj) {
+  console.log(JSON.stringify(obj));
+}
 
 const process = require('process')
 shardTests(process.argv[2], process.argv[3]);
