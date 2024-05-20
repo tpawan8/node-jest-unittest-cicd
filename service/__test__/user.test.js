3fe9d316dd1da5359bc05dc6e2fd4936f75957e9
tpawan8 <63823624+tpawan8@users.noreply.github.com>
2024-05-19 20:37:37 -0700

    add a basic playwright test

--- service/__test__/user.test.js
+++ service/__test__/user.test.js
@@ -1,27 +1,35 @@
-const userService = require('../user');
+// const userService = require('../user');
 
-describe('user service', () => {
-  test('does nothing if separation is already correct', () => {
-    const { fName, mNames, lName } = userService.sanitizeNames(
-      'John',
-      'Alexander Brian',
-      'Smith'
-    );
+// describe('user service', () => {
+//   test('does nothing if separation is already correct', () => {
+//     const { fName, mNames, lName } = userService.sanitizeNames(
+//       'John',
+//       'Alexander Brian',
+//       'Smith'
+//     );
 
-    expect(fName).toEqual('John');
-    expect(mNames).toEqual('Alexander Brian');
-    expect(lName).toEqual('Smith');
-  });
+//     expect(fName).toEqual('John');
+//     expect(mNames).toEqual('Alexander Brian');
+//     expect(lName).toEqual('Smith');
+//   });
 
-  test('correct separation of firstName if it contains spaces', () => {
-    const { fName, mNames, lName } = userService.sanitizeNames(
-      'John Alexander',
-      'Brian',
-      'Smith'
-    );
+//   test('correct separation of firstName if it contains spaces', () => {
+//     const { fName, mNames, lName } = userService.sanitizeNames(
+//       'John Alexander',
+//       'Brian',
+//       'Smith'
+//     );
 
-    expect(fName).toEqual('John');
-    expect(mNames).toEqual('Alexander Brian');
-    expect(lName).toEqual('Smith');
-  });
+//     expect(fName).toEqual('John');
+//     expect(mNames).toEqual('Alexander Brian');
+//     expect(lName).toEqual('Smith');
+//   });
+// });
+
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const name = await page.innerText('.navbar__title');
+  expect(name).toBe('Playwright');
 });
